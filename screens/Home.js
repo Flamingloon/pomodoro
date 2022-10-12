4cfe4ef0aeb350b660c8ca526ed68eaea5f6d9c8
Saheel Sapovadia <saheelsapovadia23@gmail.com>
2022-10-12 07:29:48 +0530

    navigation added

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
 
-const Home = () => {
+const Home = ({ navigation, route }) => {
   return (
     <View>
-      <Text>Home</Text>
+      <Button onPress={() => navigation.navigate("Tasks")} title="Tasks->" />
     </View>
   );
 };
